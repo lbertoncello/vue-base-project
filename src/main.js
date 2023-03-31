b22e008173c7285e0b45017a905af2c512f88839
lbertoncello <lucas_b_andrade@hotmail.com>
2023-03-31 16:01:18 -0300

    feat: add vue router support

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
+// Vue Core
 import { createApp } from 'vue';
-import './style.css';
+import registerPlugins from '@/plugins';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+// Styles
+import '@/assets/styles/style.css';
+
+const app = createApp(App);
+
+// Insert plugins and middlewares
+registerPlugins(app);
+
+app.mount('#app');
